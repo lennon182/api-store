385bdebffa00467f308b9dd76ccc73c352201a4f
E Iván Mtz Leal <eivanmtzleal@MacBook-Pro.local>
2020-06-04 13:38:51 -0500

    FIX Customer model

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,4 +1,4 @@
-const Customer = require("./../models/customer.model");
+const Customer = require("./../models/Customer.modgitl");
 
 exports.addCustomer = async (req, resp) => {
   const addCustomer = new Customer();
